7974ccaeb75a86c2811e9fee17c7bdef821c22db
replacem3nts <donny.landis@gmail.com>
2020-03-29 17:53:38 -0400

    updates file to pass tests

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Benjamin"
+    end
+
+    get '/hometown' do
+        "My hometown is Philadelphia, PA"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Yankee Doodle"
+    end
 end
