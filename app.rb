30fdca546fd0c67322738929e089fb12d8cc91f3
StephanieZou1 <stephanie.zou3@gmail.com>
2020-03-30 11:31:01 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Stephanie Zou"
+    end
+
+    get '/hometown' do
+        "My hometown is Houston"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Dreams by Fleetwood Mac"
+    end
 end
