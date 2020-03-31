d2818037b0a1f88d77ce8ab9a4a8923c37cb4e28
isabel k. lee <hello@isabelklee.co>
2020-03-31 14:39:04 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is isabel k. lee"
+    end
+
+    get '/hometown' do
+        "My hometown is new york city"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is blah blah blah"
+    end
 end
